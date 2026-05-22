030948094f62234795823706ac5896d34d4d9b55
Apratim Das <218270628+uncomparableX@users.noreply.github.com>
2026-05-22 11:00:20 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    typedRoutes: false,
+  },
+};
+
+module.exports = nextConfig;
